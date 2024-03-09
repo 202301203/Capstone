d5ef390792d9b33797016fbfefc93e5250ec5c42
202301203 <202301203@daiict.ac.in>
2024-03-09 18:18:47 +0530

    adding basic class

--- TVScheduler.cpp
+++ TVScheduler.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 #include<vector>
+#define M 15
 using namespace std;
 
+class TVSeries{
+    
+};
+
+class Member{
+    private:
+        string name;
+        vector<int> FreeSlots; //1 for free slot , 0 for busy
+        vector<string> FavoriteSeries;
+    public:
+        Member(string s) : name(s) {
+            int k;
+            cout << "Enter Free Slots of Member " << s << "(1 for free, 0 otherwise) :- ";
+            for(int i=0 ; i<M ; i++)
+            {
+                cin >> k;
+                FreeSlots.push_back(k);
+            }
+            
+        } 
+};
+
+
+
 int main(){
+
     return 0;
 }
